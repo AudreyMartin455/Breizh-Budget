658e675087a91e49f6d9ffde6232b97804f7c99a
Hembert Guillaume <guillaume.hembert@etudiant.univ-rennes1.fr>
2019-12-02 12:00:35 +0100

    Chnagement activity acceuil vers event

--- app/src/main/java/com/example/breizhbudget/Accueil.java
+++ app/src/main/java/com/example/breizhbudget/Accueil.java
@@ -1,5 +1,6 @@
 package com.example.breizhbudget;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,15 +11,33 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Accueil extends AppCompatActivity {
+
+
+    @BindView(R.id.button3) Button buttonEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        buttonEvent.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View V){
+                Intent intent = new Intent(Accueil.this,EventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
