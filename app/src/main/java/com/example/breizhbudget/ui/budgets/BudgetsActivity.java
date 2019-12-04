24c55977a6bbf96177a1d3e9d282f399016740c1
Hembert Guillaume <guillaume.hembert@etudiant.univ-rennes1.fr>
2019-12-04 10:08:24 +0100

    Modification Bottom Menu Event (icones, lien avec fragment)

--- app/src/main/java/com/example/breizhbudget/ui/budgets/BudgetsActivity.java
+++ app/src/main/java/com/example/breizhbudget/ui/budgets/BudgetsActivity.java
@@ -11,7 +11,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -37,7 +36,7 @@ public class BudgetsActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_ajoutParticipant)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
