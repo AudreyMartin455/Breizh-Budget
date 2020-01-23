718f5f319b84ec3928492e8f0b759664a7473548
Hembert Guillaume <guillaume.hembert@etudiant.univ-rennes1.fr>
2020-01-23 15:38:14 +0100

    Burger Menu fonctionel sur Acceuil et ComptageActivity

--- app/src/main/java/com/example/breizhbudget/Accueil.java
+++ app/src/main/java/com/example/breizhbudget/Accueil.java
@@ -9,8 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -53,6 +55,38 @@ public class Accueil extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.burgermenu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        switch (id){
+            case R.id.burgerMenu_Budget:
+                Intent intentBudget = new Intent(Accueil.this, BudgetsActivity.class);
+                startActivity(intentBudget);
+                return true;
+
+            case R.id.burgerMenu_Dettes:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.burgerMenu_Event:
+                Intent intentEvent = new Intent(Accueil.this, EventActivity.class);
+                startActivity(intentEvent);
+                return true;
+
+            case R.id.burgerMenu_Parametre:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+
+            case R.id.burgerMenu_Contacts:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/breizhbudget/ComptageActivity.java
+++ app/src/main/java/com/example/breizhbudget/ComptageActivity.java
@@ -4,12 +4,16 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.breizhbudget.ui.budgets.BudgetsActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -58,6 +62,35 @@ public class ComptageActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.burgermenu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        switch (id){
+            case R.id.burgerMenu_Budget:
+                Intent intentBudget = new Intent(ComptageActivity.this, BudgetsActivity.class);
+                startActivity(intentBudget);
+
+            case R.id.burgerMenu_Dettes:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+
+            case R.id.burgerMenu_Event:
+                Intent intentEvent = new Intent(ComptageActivity.this, EventActivity.class);
+                startActivity(intentEvent);
+
+            case R.id.burgerMenu_Parametre:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+
+            case R.id.burgerMenu_Contacts:
+                Toast.makeText(getApplicationContext(),"Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public  void showDta(String newString1) {
