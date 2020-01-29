e8e044ab54da9e0528c8ddebdecc0933f9e79a0a
audrey <audreymartinai@gmail.com>
2020-01-29 18:50:44 +0100

    Tricount fonctionnel

--- app/src/main/java/com/example/breizhbudget/ui/event/TricountActivity.java
+++ app/src/main/java/com/example/breizhbudget/ui/event/TricountActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import com.example.breizhbudget.R;
 import com.example.breizhbudget.Repository.RepositoryEvent;
+import com.example.breizhbudget.ui.budgets.BudgetsActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -47,6 +52,64 @@ public class TricountActivity  extends AppCompatActivity {
 
         this.repository.getAmountPerPerson(this,this.titleEvent,true);
 
+    }
+
+    /**
+     * Fonction pour afficher le bouton retour dans l'action bar
+     */
+    private void setupActionBar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Fonction du burger menu
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.burgermenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        switch (id) {
+            case R.id.burgerMenu_Budget:
+                Intent intentBudget = new Intent(TricountActivity.this, BudgetsActivity.class);
+                startActivity(intentBudget);
+                return true;
+
+            case R.id.burgerMenu_Dettes:
+                Toast.makeText(getApplicationContext(), "Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.burgerMenu_Event:
+                Intent intentEvent = new Intent(TricountActivity.this, EventActivity.class);
+                startActivity(intentEvent);
+                return true;
+
+            case R.id.burgerMenu_Parametre:
+                Toast.makeText(getApplicationContext(), "Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+
+            case R.id.burgerMenu_Contacts:
+                Toast.makeText(getApplicationContext(), "Prochainement Disponible !", Toast.LENGTH_SHORT).show();
+
+            case R.id.home: // Option pour le retour
+                Intent intentParticipant = new Intent(TricountActivity.this, ViewEvent.class);
+                intentParticipant.putExtra("title", this.titleEvent);
+                startActivity(intentParticipant);
+                return true;
+        }
+        return false;
     }
 
     public void tricount(List<Participant> participantsList){
