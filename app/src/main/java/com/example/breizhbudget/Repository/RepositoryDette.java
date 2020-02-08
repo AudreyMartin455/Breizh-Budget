82e7d7a83bdcdf3481358c6d096d283509bfe96b
audrey <audreymartinai@gmail.com>
2020-02-08 18:49:10 +0100

    Repo dettes

--- app/src/main/java/com/example/breizhbudget/Repository/RepositoryDette.java
+++ app/src/main/java/com/example/breizhbudget/Repository/RepositoryDette.java
@@ -2,11 +2,16 @@ package com.example.breizhbudget.Repository;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.example.breizhbudget.ui.dette.DettePretActivity;
 import com.example.breizhbudget.ui.dette.ModelDette;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,6 +33,65 @@ public class RepositoryDette {
             repository = new RepositoryDette();
         }
         return repository;
+    }
+
+    public void getAllDette(Context context){
+
+        List<ModelDette> detteList = new ArrayList<>();
+        ProgressDialog progressDialog = new ProgressDialog(context);
+
+        progressDialog.setTitle("loading data ....");
+        progressDialog.show();
+
+        db.collection("Dettes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        progressDialog.dismiss();
+
+
+                        for (DocumentSnapshot doc : task.getResult()) {
+                            ModelDette modelDette = new ModelDette(doc.getId(),
+                                    doc.getString("beneficiaire"),
+                                    doc.getString("description"),
+                                    doc.getDouble("montant"),
+                                    doc.getBoolean("sign"));
+                            detteList.add(modelDette);
+
+                        }
+
+                        DettePretActivity dpa = (DettePretActivity) context;
+                        dpa.updateDettesUI(detteList);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(context,e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void deleteDette(ModelDette dette,Context context){
+        db.collection("Dettes").document(dette.getId())
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(context,"Dettes deleted",Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                        getAllDette(context);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context,"Error, Dettes not deleted",Toast.LENGTH_SHORT).show();
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+
     }
     
 }
